fc411c52ddb17121f3d455c9b05ebbf735e071b8
gaoqi <1224362143@qq.com>
2023-11-03 16:44:48 +0800

    chore: 添加ts支持

--- src/extension.ts
+++ src/extension.ts
@@ -19,18 +19,19 @@ const defaultData = {
   sortText: "sortText",
   preselect: "preselect",
 };
-const getI18nTexts = async () => {
+const getI18nTexts = async (document: vscode.TextDocument) => {
   try {
     // 获取vscode配置下的解析地址以替换默认的值
     const globalPath = vscode.workspace
       .getConfiguration()
-      .get("waLanguageTipSettingPath");
+      .get("waLanguageTipSettingPath") as string;
     const globalConfigs = vscode.workspace
       .getConfiguration()
       .get("waLanguageTipGloablValue") as Record<
       "key" | "value" | "isString",
       string | boolean
     >[];
+
     let realPath = globalPath ?? defaultPath;
     const file = await vscode.workspace.findFiles(realPath as string);
     let configs: Record<string, any>[] = [];
@@ -51,11 +52,36 @@ const getI18nTexts = async () => {
         });
       });
     }
+
+    const isJson = globalPath.endsWith(".json");
     // 这里配置本地的
     if (file?.length > 0) {
-      const data = await fs.readFileSync(file[0].path, "utf-8");
-      if (data) {
-        const jsonData = JSON.parse(data);
+      let jsonData = {};
+      if (isJson) {
+        jsonData = JSON.parse(await fs.readFileSync(file[0].path, "utf-8"));
+      }
+      if (!isJson) {
+        // 当前活跃的文件夹;
+        const activeWork = vscode.workspace.getWorkspaceFolder(document.uri)
+          ?.uri.path;
+        const fileUri = vscode.Uri.joinPath(
+          vscode.Uri.file(activeWork!),
+          globalPath
+        );
+        let fileContent = (await vscode.workspace.fs.readFile(fileUri))
+          .toString()
+          .replace("export default", "")
+          .replace("module.export = ", "");
+
+        fileContent = fileContent.replace(/['"]/g, function (match) {
+          return match === "'" ? '"' : "'";
+        });
+        if (fileContent[fileContent.length - 1] === ",") {
+          fileContent = fileContent.slice(0, -1);
+        }
+        jsonData = JSON.parse(fileContent);
+      }
+      if (jsonData && typeof jsonData === "object") {
         Object.entries(jsonData).forEach(([oldKey, oldValue]) => {
           triggers.push(String(oldValue));
           // 这里转换的是 key => '${value}'
@@ -83,8 +109,8 @@ export async function activate(context: vscode.ExtensionContext) {
   const completionProvider = vscode.languages.registerCompletionItemProvider(
     LANGUAGES,
     {
-      async provideCompletionItems() {
-        const i18nTexts = await getI18nTexts();
+      async provideCompletionItems(document: vscode.TextDocument) {
+        const i18nTexts = await getI18nTexts(document);
         // ProviderResult<CompletionItem[] | CompletionList<CompletionItem>>
         return i18nTexts as vscode.ProviderResult<
           vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>
